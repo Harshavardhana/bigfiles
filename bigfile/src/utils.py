e3c26ff1dca8c5c176e7cb55f6336741f1b29b1b
Harshavardhana <fharshav@redhat.com>
2013-09-04 01:28:01 -0700

    Add python CDLL import into bigfile shell
    Signed-off-by: Harshavardhana <fharshav@redhat.com>

--- bigfile/src/utils.py
+++ bigfile/src/utils.py
@@ -10,20 +10,26 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 # implied.
-#
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
 # Author: Harshavardhana <fharshav@redhat.com>
 #
 
-class BFException(Exception):
+import ctypes
+import ctypes.util
+import os
+import sys
+import time
+import types
 
-    def __init__(self, msg, err):
-        Exception.__init__(self, msg)
-        Exception.__init__(self, err)
-        self.msg = msg
-        self.err = err
-
-    def __str__(self):
-        return '%s: %s' % (self.msg, self.err)
+def load_library(lname):
+    lib = ctypes.util.find_library(lname)
+    try:
+        lib_ref = ctypes.CDLL(dll)
+    except ImportError:
+        print('failed to find lib%s. Please install lib%s' %
+              (lname, lname))
+        raise
+    finally:
+        return lib_ref
