6ae42b02e5b222ff2b87a57e6254b59b0fc9f2fd
Harshavardhana <fharshav@redhat.com>
2013-09-28 16:38:21 -0700

    More migration support to 'cmake'
    Signed-off-by: Harshavardhana <fharshav@redhat.com>

---
+++ DefineOptions.cmake
@@ -0,0 +1,11 @@
+option(WITH_GFAPI "Build with GlusterFS API support" ON)
+option(WITH_S3API "Build with Amazon S3 API support" ON)
+option(WITH_STATIC_LIB "Build with a static library" OFF)
+option(WITH_DEBUG_CALLTRACE "Build with calltrace debug output" ON)
+option(WITH_INTERNAL_DOC "Compile doxygen internal documentation" OFF)
+option(WITH_TESTING "Build with unit tests" OFF)
+option(WITH_EXAMPLES "Build examples" ON)
+
+if (WITH_TESTING)
+  set(WITH_STATIC_LIB ON)
+endif (WITH_TESTING)
